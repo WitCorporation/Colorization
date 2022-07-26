6bee710b45ccad8fdfc813e501c3511a14a3188a
Sirizis <104469295+Sirizis@users.noreply.github.com>
2022-07-26 17:10:08 +0300

    Добавил модель

---
+++ Assets/Script/aa.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aa : MonoBehaviour
+{
+    public int[] array;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(array[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
